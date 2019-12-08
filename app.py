c58ab43eab5e3d7934a7078b21ebc85e168a0669
Siddhant Shinde <50303097+siddhant1703@users.noreply.github.com>
2019-12-08 18:43:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,17 +20,37 @@ class Departments_Meta(Resource):
         query = conn.execute("select distinct DEPARTMENT from salaries")
         return {'departments': [i[0] for i in query.cursor.fetchall()]}
 
-class Departmental_Salary(Resource):
+
+class Departmental_pos(Resource):
     def get(self, department_name):
         conn = e.connect()
-        query = conn.execute("select * from salaries where Department='%s'"%department_name.upper())
+        query = conn.execute("select distinct POSITION from salaries where Department='%s'"%department_name.upper())
+        result = {'positions': [i[0] for i in query.cursor.fetchall()]}
+        return result
+        #We can have PUT,DELETE,POST here. But in our API GET implementation is sufficient
+
+
+class Departmental_pos_name(Resource):
+    def get(self, department_name, pos_name):
+        conn = e.connect()
+        query = conn.execute("select distinct NAME from salaries where Department='%s'"%department_name.upper() + "AND Position='%s'"%pos_name.upper())
+        result = {'name': [i[0] for i in query.cursor.fetchall()]}
+        return result
+        #We can have PUT,DELETE,POST here. But in our API GET implementation is sufficient
+
+
+class Departmental_pos_name_sal(Resource):
+    def get(self, department_name, pos_name, name):
+        conn = e.connect()
+        query = conn.execute("select * from salaries where Department='%s'"%department_name.upper() + "AND Position='%s'"%pos_name.upper() + "AND Name='%s'"%name.upper())
         #Query the result and get cursor.Dumping that data to a JSON is looked by extension
         result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return result
         #We can have PUT,DELETE,POST here. But in our API GET implementation is sufficient
 
- 
-api.add_resource(Departmental_Salary, '/dept/<string:department_name>')
+api.add_resource(Departmental_pos_name_sal, '/dept/<string:department_name>/<string:pos_name>/<string:name>')
+api.add_resource(Departmental_pos_name, '/dept/<string:department_name>/<string:pos_name>')
+api.add_resource(Departmental_pos, '/dept/<string:department_name>')
 api.add_resource(Departments_Meta, '/departments')
 
 if __name__ == '__main__':
